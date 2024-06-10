80bf57560ccb740547383f090ecdf4378846ed15
ssoo1234 <saeho0403@gmail.com>
2024-06-10 15:28:23 +0900

    add Calendar
    add Calendar

---
+++ src/components/page/Calendar.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <VCalendar expanded />
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Calendar"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/routes/page/page.js
@@ -0,0 +1,9 @@
+import Calendar from "@/views/page/CalendarView.vue";
+
+export default [
+    {
+        path: "/page/calendar",
+        name: 'Calendar',
+        component: Calendar
+    }
+];
