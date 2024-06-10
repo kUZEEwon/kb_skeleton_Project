ad2864be6caa2d0273f74f1c6e8613ed7be0e46c
ssoo1234 <saeho0403@gmail.com>
2024-06-10 15:44:50 +0900

    chore
    chore

---
+++ src/views/page/OverView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <Home />
+    </div>
+</template>
+
+<script>
+import Home from '@/components/page/Home.vue';
+export default {
+    components: {
+        Home
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
