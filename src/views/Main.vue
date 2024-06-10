e5adfe9335f3ec2b20110c65b383231fd8ce1e88
ssoo1234 <saeho0403@gmail.com>
2024-06-10 14:45:39 +0900

    set routes

---
+++ src/views/Main.vue
@@ -0,0 +1,17 @@
+<template>
+    <!-- <Header></Header> -->
+    <router-view />
+</template>
+
+<script>
+// import Header from '../components/Header.vue';
+
+export default {
+    name: "Main",
+    components: {
+        Header
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
