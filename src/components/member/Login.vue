33caf5cbb9e6a44906c4eec070220c5ff127d2e7
wjdgus9808 <dbswjdgus9808@gmail.com>
2024-06-11 10:51:28 +0900

    fix: 로그인 버그 수정

--- src/components/member/Login.vue
+++ src/components/member/Login.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>Sign in</h1>
-        <form>
+        <form @submit.prevent>
             <p>
                 <input class="inputVal" v-model.trim="id" type="text" placeholder="Enter your ID" />
             </p>
@@ -10,8 +10,8 @@
                     placeholder="Enter your PW" />
             </p>
             <p>
-                <button @click=signIn()>Sign in</button>
-                <button @click=signUp()>Sign Up</button>
+                <button type="submit" @click="signIn">Sign in</button>
+                <button @click="signUp">Sign Up</button>
             </p>
         </form>
     </div>
@@ -31,8 +31,8 @@ const router = useRouter();
 const signIn = async () => {
     const response = await axios.get('http://localhost:3001/users', { params: { "id": id.value, "password": password.value } })
     if (response.data[0].password == password.value) {
-        cookies.set('id', id.value);
-        router.push('/');
+        cookies.set('id', id.value, { path: '/' });
+        router.push(router.currentRoute.value.redirectedFrom.fullPath);
     } else {
         alert("잘못된 비밀번호입니다.")
         id.value = ''
@@ -47,12 +47,11 @@ const signUp = () => {
 <style scoped>
 .inputVal {
     border-radius: 8px;
-    border: 1px solid transparent;
+    border: 1px solid rgba(255, 255, 255, 0.413);
     padding: 0.6em 1.2em;
     font-size: 1em;
     font-weight: 500;
     font-family: inherit;
-    background-color: #1a1a1a;
     cursor: text;
     transition: border-color 0.25s;
 }
