5b1d2e6d985172c8bcedb1284f3f68f4c2dadd48
wjdgus9808 <dbswjdgus9808@gmail.com>
2024-06-12 11:28:44 +0900

    feat: Add About

--- src/views/page/AboutView.vue
+++ src/views/page/AboutView.vue
@@ -1,20 +1,16 @@
 <template>
+    <div class="calendar-container">
+        <button @click="prevMonth">〈</button>
+        <span>{{ formattedDate }}</span>
+        <button @click="nextMonth">〉</button>
+    </div>
     <section>
-        <!--for demo wrap-->
-        <h1>Fixed Table header</h1>
         <div class="tbl-header">
             <table>
                 <thead>
                     <tr>
                         <th style="color: blue;">날짜</th>
-                        <th style="color: blue;">
-                            <select v-model="selectedCategory">
-                                <option value="">전체</option> <!-- 전체 옵션 -->
-                                <option v-for="category in categories" :key="category.id" :value="category.name">
-                                    {{ category.name }}
-                                </option>
-                            </select>
-                        </th>
+                        <th style="color: blue;">카테고리</th>
                         <th style="color: blue;">메모</th>
                         <th style="color: blue;">수입</th>
                     </tr>
@@ -24,7 +20,7 @@
         <div class="tbl-content">
             <table cellpadding="0" cellspacing="0" border="0">
                 <tbody>
-                    <tr v-for="data in outcome" :key="data.id">
+                    <tr v-for="data in filteredIncome" :key="data.id">
                         <td>{{ new Date(data.date).getDate() }}일</td>
                         <td>{{ data.category }}</td>
                         <td>{{ data.memo }}</td>
@@ -37,7 +33,12 @@
 
     <section>
         <!--for demo wrap-->
-        <h1>Fixed Table header</h1>
+        <select v-model="selectedCategory">
+            <option value="">전체</option> <!-- 전체 옵션 -->
+            <option v-for="category in categories" :key="category.id" :value="category.name">
+                {{ category.name }}
+            </option>
+        </select>
         <div class="tbl-header">
             <table>
                 <thead>
@@ -53,7 +54,7 @@
         <div class="tbl-content">
             <table cellpadding="0" cellspacing="0" border="0">
                 <tbody>
-                    <tr v-for="data in outcome" :key="data.id">
+                    <tr v-for="data in filteredOutcome" :key="data.id">
                         <td>{{ new Date(data.date).getDate() }}일</td>
                         <td>{{ data.category }}</td>
                         <td>{{ data.memo }}</td>
@@ -66,12 +67,12 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue';
+import { computed, onMounted, ref } from 'vue';
 import { getCookie } from '../../auth/AuthService';
 import axios from 'axios';
 
-const income = ref('')
-const outcome = ref('')
+const income = ref([])
+const outcome = ref([])
 const selectedCategory = ref('');
 const categories = ref([
     { id: 1, name: '식비' },
@@ -82,19 +83,58 @@ const categories = ref([
     { id: 6, name: '전자기기' },
     { id: 6, name: '문화생활' }
 ]);
+const currentDate = ref(new Date());
+
+// 이전 월로 이동하는 함수
+const prevMonth = () => {
+    currentDate.value = new Date(currentDate.value.setMonth(currentDate.value.getMonth() - 1));
+};
+
+// 다음 월로 이동하는 함수
+const nextMonth = () => {
+    currentDate.value = new Date(currentDate.value.setMonth(currentDate.value.getMonth() + 1));
+};
+// 현재 날짜를 형식에 맞게 표시하는 함수
+const formattedDate = computed(() => {
+    const options = { year: 'numeric', month: 'long' };
+    return currentDate.value.toLocaleDateString('ko-KR', options);
+
+});
 onMounted(async () => {
     try {
         const response = await axios.get('http://localhost:3001/account', { params: { "uid": getCookie() } });
-
-        console.log(response.data)
         income.value = response.data.filter(item => item.income).sort((a, b) => new Date(b.date) - new Date(a.date))
         outcome.value = response.data.filter(item => !item.income).sort((a, b) => new Date(b.date) - new Date(a.date))
-        console.log(income.value)
-        console.log(outcome.value)
     } catch (error) {
         console.error('데이터 가져오기 오류:', error);
     }
-})
+});
+
+/* 날짜별 filter*/
+const filteredIncome = computed(() => {
+    return income.value.filter(item => {
+        const date = new Date(item.date);
+        return date.getMonth() + 1 == currentDate.value.getMonth() + 1
+    });
+
+});
+
+/* 날짜, 카테고리별 Filter*/
+const filteredOutcome = computed(() => {
+    if (!selectedCategory.value) {
+        return outcome.value.filter(item => {
+            const date = new Date(item.date);
+            return date.getMonth() + 1 == currentDate.value.getMonth() + 1
+        }); // 전체 카테고리일 경우 모든 데이터 표시
+    } else {
+        return outcome.value.filter(item => {
+            const date = new Date(item.date);
+            console.log('date= ', date.getMonth())
+            console.log('current= ', currentDate.value.getMonth())
+            return item.category === selectedCategory.value && date.getMonth() + 1 == currentDate.value.getMonth() + 1
+        });
+    }
+});
 </script>
 
 <style scoped>
@@ -141,11 +181,36 @@ td {
     border-bottom: solid 1px rgba(255, 255, 255, 0.1);
 }
 
-
-/* demo styles */
-
-
 section {
     margin: 50px;
+}
+
+.calendar-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    /* 배경 색상 */
+    padding: 10px;
+    border-radius: 10px;
+    color: aaa;
+    /* 텍스트 색상 */
+    font-size: 20px;
+}
+
+button {
+    background: none;
+    border: none;
+    color: #aaa;
+    /* 버튼 텍스트 색상 */
+    font-size: 24px;
+    cursor: pointer;
+    margin: 0 20px;
+    /* 버튼 간격 */
+}
+
+button:hover {
+    color: #aaaaaa88;
+    /* 버튼 호버 색상 */
 }
 </style>
