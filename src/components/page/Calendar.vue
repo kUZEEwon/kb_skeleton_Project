f12a631224f95ae1cdb23627b1eb71f851ea04bc
ssoo1234 <saeho0403@gmail.com>
2024-06-12 11:24:50 +0900

    feat calendar
    수정 삭제 추가

--- src/components/page/Calendar.vue
+++ src/components/page/Calendar.vue
@@ -5,13 +5,14 @@
 
         <table v-if="selectDayData.transaction.length !== 0">
             <thead>
-                <th><input type="checkbox" value="all" v-model="allChecked"></th>
+                <th v-if="!updating"><input type="checkbox" value="all" v-model="allChecked"></th>
+                <th v-else><input type="checkbox" value="all" v-model="allChecked" disabled></th>
                 <th>category</th>
                 <th>cost</th>
                 <th>income</th>
                 <th>memo</th>
             </thead>
-            <tbody>
+            <tbody v-if="!updating">
                 <tr v-for="data in selectDayData.transaction" :key="data.id">
                     <td><input type="checkbox" :id="data.id" :value="data.id" v-model="checked" :key="data.id"></td>
                     <td>{{ data.category }}</td>
@@ -19,13 +20,45 @@
                     <td>{{ data.income }}</td>
                     <td>{{ data.memo }}</td>
                 </tr>
+                <tr v-if="checked.length !== 0">
+                    <td colspan="5">
+                        <button @click="setUpdate()">Update</button>
+                        <button @click="deleteTransaction()">Delete</button>
+                    </td>
+                </tr>
+            </tbody>
+            <tbody v-else>
+                <tr v-for="data in selectDayData.transaction" :key="data.id">
+                    <td><input type="checkbox" :id="data" :value="data.id" v-model="checked"
+                            :key="data.id" disabled></td>
+                    <td v-if="checkId(data.id)"><select :id="data" :value="data.category" v-model="data.category"
+                            :key="data.id">
+                            <option v-for="category in categorys" :key="category.index">
+                                {{ category }}
+                            </option>
+                        </select></td>
+                    <td v-else>{{ data.category }}</td>
+                    <td v-if="checkId(data.id)"><input type="number" :id="data" :value="data.cost" v-model="data.cost"
+                            :key="data.id"></td>
+                    <td v-else>{{ data.cost }}</td>
+                    <td v-if="checkId(data.id)"><select :id="data" :value="data.income" v-model="data.income"
+                            :key="data.id">
+                            <option value="true">수입</option>
+                            <option value="false">지출</option>
+                        </select></td>
+                    <td v-else>{{ data.income }}</td>
+                    <td v-if="checkId(data.id)"><input type="text" :id="data" :value="data.memo" v-model="data.memo"
+                            :key="data.id"></td>
+                    <td v-else>{{ data.memo }}</td>
+                </tr>
+                <tr>
+                    <td colspan="5">
+                        <button @click="updateTransaction()">modification completed</button>
+                        <button @click="setUpdate()">cancle</button>
+                    </td>
+                </tr>
             </tbody>
         </table>
-
-        <div v-if="checked.length !== 0">
-            <button @click="">Update</button>
-            <button @click="deleteTransaction()">Delete</button>
-        </div>
     </div>
 </template>
 
@@ -77,22 +110,50 @@ onBeforeUpdate(() => {
 
 function deleteTransaction() {
     checked.value.forEach(async (id) => {
-        // 거래내역 아이디 중 일치하는게 있나 확인
-        if (transactionData.transaction.map(v => v.id).includes(id)) {
-            // 일치하는 아이디의 인덱스 반환후 삭제
-            const index = transactionData.transaction.findIndex((v) => v.id === id);
-            transactionData.transaction.splice(index, 1);
-            
-            try {
-                const response = await axios.delete(`http://localhost:3001/account/${id}`, { data: {} });
-                console.log("deleted");
-            } catch (error) {
-                console.error('Error fetching data:', error);
-                checked.value.splice(0);
-            }
+        // 일치하는 아이디의 인덱스 반환후 삭제
+        const index = transactionData.transaction.findIndex((v) => v.id === id);
+        transactionData.transaction.splice(index, 1);
+
+        try {
+            const response = await axios.delete(`http://localhost:3001/account/${id}`, { data: {} });
+            console.log("deleted");
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            checked.value.splice(0);
         }
-    })
+    }
+    )
     checked.value.splice(0);
+}
+
+const updating = ref(false);
+const categorys = ["식비", "교통비", "쇼핑", "의료비", "여행", "취미", "문화생활", "전자기기"];
+
+function updateTransaction() {
+    checked.value.forEach(async (id) => {
+        // 일치하는 아이디의 인덱스 반환
+        const index = transactionData.transaction.findIndex((v) => v.id === id);
+        // console.log(transactionData.transaction[index]);
+
+        try {
+            const response = await axios.put(`http://localhost:3001/account/${id}`, transactionData.transaction[index]);
+            console.log("put");
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            checked.value.splice(0);
+        }
+    }
+    )
+    checked.value.splice(0);
+    updating.value = false;
+}
+
+function setUpdate() {
+    updating.value = !updating.value;
+}
+
+function checkId(id) {
+    return checked.value.includes(id);
 }
 
 let selectedDate = "";
