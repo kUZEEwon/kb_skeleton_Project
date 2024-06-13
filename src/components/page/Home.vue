128be404e3cd996f396c62b86498dd5f4aa9f8f4
kUZEEwon <pink7519@pusan.ac.kr>
2024-06-13 14:41:36 +0900

    병합 오류 수정

--- src/components/page/Home.vue
+++ src/components/page/Home.vue
@@ -47,6 +47,18 @@ export default {
         TransactionTable,
         AddTransaction,
     },
+    data() {
+        return {
+            currentYear: new Date().getFullYear(),
+            currentMonth: new Date().getMonth() + 1,
+            categoryData: [],
+            transactionData: [],
+            category: [],
+            items: [],
+            modalCheck: false,
+            intervalId: null
+        };
+    },
     computed: {
         prevMonthLabel() {
             return (this.currentMonth === 1 ? 12 : this.currentMonth - 1) + "월";
@@ -63,60 +75,12 @@ export default {
             return this.calc(this.category);
         }
     },
-    data() {
-        return {
-            currentYear: new Date().getFullYear(),
-            currentMonth: new Date().getMonth() + 1,
-            categoryData: [],
-            transactionData: [],
-            category: [],
-            items: [],
-            modalCheck: false,
-            intervalId: null
-        };
-    },
+
     watch: {
         currentYear: 'fetchData',
         currentMonth: 'fetchData'
     },
-    mounted() {
-        this.fetchData();
-    },
-    methods: {
-        async fetchData() {
-            try {
-                const response = await axios.get('http://localhost:3001/account');
-                const data = response.data;
-                const cookieId = this.$cookies.get('id');
-
-                // 이번 달 데이터 필터링
-                const filteredData = data.filter(item => {
-                    const date = new Date(item.date);
-                    return date.getFullYear() === this.currentYear && date.getMonth() + 1 === this.currentMonth;
-                });
-
-                // 이전 달 데이터 필터링
-                const previousMonth = this.currentMonth === 1 ? 12 : this.currentMonth - 1;
-                const previousYear = this.currentMonth === 1 ? this.currentYear - 1 : this.currentYear;
-                const filteredPrevMonthData = data.filter(item => {
-                    const date = new Date(item.date);
-                    return date.getFullYear() === previousYear && date.getMonth() + 1 === previousMonth;
-                });
-
-                this.transactionData = filteredData;
-                this.items = data;
-
-                // store의 fetchData 액션 호출
-                await useAccountStore().fetchData(filteredData, filteredPrevMonthData);
-                useAccountStore().setItems(data.filter(item => item.uid == cookieId));
-
-                this.updateCategoryData(filteredData);
-                this.category = this.reduceByCategory(filteredData);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        },
-
+    
         mounted() {
             this.fetchData();
 
@@ -250,8 +214,8 @@ export default {
                 this.$emit('updateTableData', this.category); // 이벤트 발생
             },
         },
-    },
-}
+    };
+
 
 </script>
 
