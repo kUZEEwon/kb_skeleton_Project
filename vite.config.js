450801fdfba4daa7a89650e6cb9551392e24b4ac
GUZEE <98088494+kUZEEwon@users.noreply.github.com>
2024-06-22 17:08:51 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/actions-test/src',
+  base: '/kb_skeleton_Project/src',
   resolve: {
     alias: [
       { find: "@", replacement: path.resolve(__dirname, __dirname + "/src") },
