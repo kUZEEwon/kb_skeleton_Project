f9f9aeab7e263e0afbbdcd2e9598920dd22edbfe
kUZEEwon <pink7519@pusan.ac.kr>
2024-06-22 16:15:18 +0900

    vite.config.js에 base path 추가

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base:"/kb_skeleton_Project/",
   resolve: {
     alias: [
       { find: "@", replacement: path.resolve(__dirname, __dirname + "/src") },
